f404aac4698f613fb3ea7c43d8ee0cc3943901f5
uglyDwarf <f.josef@email.cz>
2018-12-02 21:09:46 +0100

    Adding implementation of SandyBarbourUtilities module.

---
+++ XPython/sb.c
@@ -0,0 +1,85 @@
+#define _GNU_SOURCE
+#include <Python.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+static PyObject *SandyBarbourDisplayFun(PyObject *self, PyObject *args)
+{
+  (void) self;
+  const char *msg;
+  if(!PyArg_ParseTuple(args, "s", &msg)){
+    return NULL;
+  }
+  //TODO
+  printf("SBD: %s\n", msg);
+  Py_RETURN_NONE;
+}
+
+static PyObject *SandyBarbourClearDisplayFun(PyObject *self, PyObject *args)
+{
+  (void) self;
+  (void) args;
+  //TODO
+  printf("SBD:======================================\n");
+  Py_RETURN_NONE;
+}
+
+static PyObject *SandyBarbourPrintFun(PyObject *self, PyObject *args)
+{
+  (void) self;
+  const char *msg;
+  if(!PyArg_ParseTuple(args, "s", &msg)){
+    return NULL;
+  }
+  //TODO
+  printf("SBP: %s\n", msg);
+  Py_RETURN_NONE;
+}
+
+static PyObject *SandyBarbourClearPrintFun(PyObject *self, PyObject *args)
+{
+  (void) self;
+  (void) args;
+  //TODO
+  printf("SBP:======================================\n");
+  Py_RETURN_NONE;
+}
+
+static PyObject *cleanup(PyObject *self, PyObject *args)
+{
+  (void) self;
+  (void) args;
+  Py_RETURN_NONE;
+}
+
+static PyMethodDef SBUMethods[] = {
+  {"SandyBarbourDisplay", SandyBarbourDisplayFun, METH_VARARGS, ""},
+  {"SandyBarbourClearDisplay", SandyBarbourClearDisplayFun, METH_VARARGS, ""},
+  {"SandyBarbourPrint", SandyBarbourPrintFun, METH_VARARGS, ""},
+  {"SandyBarbourClear", SandyBarbourClearPrintFun, METH_VARARGS, ""},
+  {"cleanup", cleanup, METH_VARARGS, ""},
+  {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef SBUModule = {
+  PyModuleDef_HEAD_INIT,
+  "SandyBarbourUtilities",
+  NULL,
+  -1,
+  SBUMethods,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+PyMODINIT_FUNC
+PyInit_SBU(void)
+{
+  PyObject *mod = PyModule_Create(&SBUModule);
+  return mod;
+}
+
+
+
+
