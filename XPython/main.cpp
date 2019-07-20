b62faf8ee0aec716e2b07a46174677f807afda5a
uglyDwarf <f.josef@email.cz>
2019-07-20 20:15:25 +0200

    Reshuffled the testloop.
    Fixed problem caused by the reload after the last test round.

--- XPython/main.cpp
+++ XPython/main.cpp
@@ -115,13 +115,13 @@ int main(int argc, char *argv[])
     if(c % 50 == 0){
       std::cout << c << std::endl;
     }
-    XPluginReceiveMessage(5, 103, (void*)333);
-    XPluginDisable();
-    XPluginEnable();
-    if(((c + 1) % 100) == 0){
+    if((c > 0) && ((c % 100) == 0)){
       std::cout << "Reloading..." << std::endl;
       XPLMCommandOnce(reloadCmd);
     }
+    XPluginReceiveMessage(5, 103, (void*)333);
+    XPluginDisable();
+    XPluginEnable();
   }
   XPluginStop();
   std::cout << "===============================================" << std::endl;
